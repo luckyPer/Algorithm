ccb0c50b86126a089235e88c4c4b7e18522a441f
pan <pzj.lovers@hotmail.com>
2019-12-27 23:01:47 +0800

    mock.

--- Problems/codeup.c
+++ Problems/codeup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define maxsize 100000
 
@@ -8,7 +9,8 @@ int main(int argc, char const *argv[])
 
     //printf(" result : %d ", getCallatz(3));
     //setSchoolInfor_B1032(2);
-    drawSquare(10, 'a');
+    //drawSquare(10, 'a');
+    printf("%d", getDifference(20181214, 20191214));
     system("pause");
     return 0;
 }
@@ -114,12 +116,80 @@ void drawSquare(int n, char a)
     }
 }
 
-
 /*
- *@description: codeup 1928 日期差值
+ *@description: codeup 1928 日期差值  时间格式   YYYY MM DD
  *@params1: 
  *@params2: 
  *@return: 
  *@date: 2019-12-27 19:11:49
 */
+bool isLeap(int date)
+{
+    int year = getYear(date);
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    {
+        return true;
+    }
+    return false;
+}
 
+int month[13][2] = {
+    //平闰年
+    {0, 1},
+    {31, 31},
+    {28, 29},
+    {31, 31},
+    {30, 30},
+    {31, 31},
+    {30, 30},
+    {31, 31},
+    {31, 31},
+    {30, 30},
+    {31, 31},
+    {30, 30},
+    {31, 31},
+};
+int getYear(int date)
+{
+    return date / 10000;
+}
+
+int getMonth(int date)
+{
+    return date % 10000 / 100;
+}
+
+int getDay(int date)
+{
+    return date % 100;
+}
+
+int getDifference(int date1, int date2)
+{
+    int Difference = 0;
+
+    int d1 = getDay(date1);
+    int d2 = getDay(date2);
+    int m1 = getMonth(date1);
+    int m2 = getMonth(date2);
+    int y1 = getYear(date1);
+    int y2 = getYear(date2);
+    while (d1 < d2 || m1 < m2 || y1 < y2)   //让小的日期的天数自增, 直到等于对应month[m1] 的天数, 在让m1自增, d1置为1; 当m1等于13的时候
+    //y1 自增, m1 置为1
+    {
+        d1++;
+        if (d1 == month[m1][isLeap(y1)] + 1)
+        {
+            m1++;
+            d1 = 1;
+        }
+        if (m1 == 13)
+        {
+            y1++;
+            m1 = 1;
+        }
+
+        Difference++;
+    }
+    return Difference;
+}
